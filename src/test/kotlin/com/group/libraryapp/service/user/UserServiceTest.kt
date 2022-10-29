066478b1ceedee0a68a16f0f3f4b7400186995e4
yeonha99 <rhdqn1207@naver.com>
2022-10-29 17:53:18 +0900

    feat:유저 관련 기능 테스트 작성하기

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,9 +1,13 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -24,5 +28,50 @@ class UserServiceTest @Autowired constructor(
         assertThat(results).hasSize(1)
         assertThat(results[0].name).isEqualTo("최태현")
         assertThat(results[0].age).isNull()
+    }
+
+    @Test
+    fun getUsersTest(){
+
+        userRepository.saveAll(listOf(
+            User("A",20),
+            User("B",null),
+        ))
+
+        val results=userService.getUsers()
+
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("A","B")
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(20,null)
+
+    }
+
+    @AfterEach
+    fun clean(){
+        userRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("유저 네임 업데이트")
+    fun updateUserNameTest(){
+
+        val savedUser=userRepository.save(User("A",null))
+        val request=UserUpdateRequest(savedUser.id,"B")
+
+        userService.updateUserName(request)
+
+        val result=userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+
+    }
+
+    @Test
+    fun deleteUserTest(){
+
+        userRepository.save(User("A",null))
+
+        userService.deleteUser("A")
+
+        assertThat(userRepository.findAll()).isEmpty()
     }
 }
