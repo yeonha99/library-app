1da9e79c3d16b065e9da4ab93e9c0d62cf7f3b8e
yeonha99 <rhdqn1207@naver.com>
2022-10-29 18:18:04 +0900

    feat:책 관련 기능 테스트 작성하기

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,6 +1,7 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
 import com.group.libraryapp.domain.user.User;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,6 +24,17 @@ public class UserLoanHistory {
 
   private boolean isReturn;
 
+
+
+  @NotNull
+  public User getUser() {
+    return user;
+  }
+
+  public boolean isReturn() {
+    return isReturn;
+  }
+
   public UserLoanHistory() {
 
   }
@@ -33,6 +45,7 @@ public class UserLoanHistory {
     this.isReturn = isReturn;
   }
 
+  @NotNull
   public String getBookName() {
     return this.bookName;
   }
---
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,97 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import com.group.libraryapp.dto.book.request.BookReturnRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+)
+{
+    @Test
+    @DisplayName("책 등록이 정상 동작한다.")
+    fun saveBookTest(){
+
+        val request=BookRequest("이상한 나라의 엘리스")
+
+        bookService.saveBook(request)
+
+        val books=bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("이상한 나라의 엘리스")
+    }
+
+    @Test
+    @DisplayName("책 대출이 정상 동작한다.")
+    fun loanBookTest(){
+
+        bookRepository.save(Book("이상한 나라의 엘리스"))
+        val savedUser=userRepository.save(User("최태현",null))
+        val request=BookLoanRequest("최태현","이상한 나라의 엘리스")
+
+        bookService.loanBook(request)
+
+        val results=userLoanHistoryRepository.findAll()
+
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("이상한 나라의 엘리스")
+        assertThat(results[0].user.id).isEqualTo(savedUser.id)
+        assertThat(results[0].isReturn).isFalse
+
+    }
+
+    @Test
+    @DisplayName("책이 이미 대출상태라면 실패한다.")
+    fun loanBookFailTest(){
+        bookRepository.save(Book("이상한 나라의 엘리스"))
+        val savedUser=userRepository.save(User("최태현",null))
+
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser,"이상한 나라의 엘리스",false))
+        val request=BookLoanRequest("최태현","이상한 나라의 엘리스")
+
+        var message=assertThrows<IllegalArgumentException> {
+            bookService.loanBook(request)
+        }.message
+        assertThat(message).isEqualTo("진작 대출되어 있는 책입니다")
+    }
+
+    @Test
+    @DisplayName("책 반납이 정상 동작한다.")
+    fun returnBookTest(){
+        bookRepository.save(Book("이상한 나라의 엘리스"))
+        val savedUser=userRepository.save(User("최태현",null))
+
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser,"이상한 나라의 엘리스",false))
+        val request=BookReturnRequest("최태현","이상한 나라의 엘리스")
+
+        bookService.returnBook(request)
+
+       val results=userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].isReturn).isTrue
+    }
+
+    @AfterEach
+    fun clean(){
+        userRepository.deleteAll()
+        bookRepository.deleteAll()
+    }
+
+}
