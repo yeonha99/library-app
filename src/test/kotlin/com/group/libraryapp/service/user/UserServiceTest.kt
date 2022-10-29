42d9658da63c9a0e3e55add27491d2dba1cbda6a
yeonha99 <rhdqn1207@naver.com>
2022-10-29 17:42:41 +0900

    feat:jnuit으로 SpringBoot 테스트하기

---
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,28 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+    private val userRepository: UserRepository,
+    private val userService: UserService,
+)
+{
+    @Test
+    fun saveUserTest(){
+        val request = UserCreateRequest("최태현",null);
+
+        userService.saveUser(request)
+
+        val results=userRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("최태현")
+        assertThat(results[0].age).isNull()
+    }
+}
